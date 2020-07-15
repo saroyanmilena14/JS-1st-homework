f1e111aca930ceda61bce317a7725642a5b45e04
Milena <64065041+saroyanmilena14@users.noreply.github.com>
2020-07-15 12:34:01 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+const strnum1= prompt('Enter your first number');
+const num1=parseInt(strnum1);
+const strnum2= prompt('Enter your second number');
+const num2=parseInt(strnum2);
+
+alert('The answer is ' + (num1+num2));
+
